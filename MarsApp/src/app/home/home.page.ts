2faf77ff2e9419cbdcebd43f55a7d03198390a82
Chinnakrit Channok <chinnakrit.pond@gmail.com>
2019-11-29 11:40:20 +0700

    add loading homepage

--- MarsApp/src/app/home/home.page.ts
+++ MarsApp/src/app/home/home.page.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
 
   constructor() {}
+
+  hidevalue = false;
+
+  ngOnInit() {
+    setTimeout(x => {
+      this.hidevalue = true;
+    }, 3000);
+  }
 
 }
